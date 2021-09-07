f632f62901e2d0bf4d3785a140d9c28d2b2ea338
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-09-07 22:44:28 +0530

    getting text value

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var button = document.querySelector("#button");
 console.log(button);
+var textArea = document.querySelector("#text-area");
 
 button.addEventListener("click", function callback() {
   console.log("clicked");
+  console.log(textArea.value);
 });
