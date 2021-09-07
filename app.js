3af65f604edd78c78e1dcad217863e138de09c2b
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-09-08 00:26:31 +0530

    docs: getting translated text

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var button = document.querySelector("#button");
-console.log(button);
-var textArea = document.querySelector("#text-area");
+var inputTextArea = document.querySelector("#input-text-area");
+var outputText = document.querySelector("#output-text");
 
+//api Url
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+// function to take input and change the url
+function getTranslatedText(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+// function to handle error
+function errorHandler(error) {
+  console.log("There is an error, please try again after some time");
+}
+
+//Event Handler for click event
 button.addEventListener("click", function callback() {
-  console.log("clicked");
-  console.log(textArea.value);
+  // .value saves the value of input
+  var inputText = inputTextArea.value;
+
+  fetch(getTranslatedText(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      // consoling json helps to decide the code
+      var translatedText = json.contents.translated;
+      outputText.innerText = translatedText;
+    })
+    .catch(errorHandler);
 });
